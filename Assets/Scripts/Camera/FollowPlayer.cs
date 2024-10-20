3b11420206d9ef67a56bc71d4411ecdc4b15afae
ThreeCoffes <leonard.madzielewski@gmail.com>
2024-10-20 16:38:26 +0200

    velocity based zoom

--- Assets/Scripts/Camera/FollowPlayer.cs
+++ Assets/Scripts/Camera/FollowPlayer.cs
@@ -6,13 +6,20 @@ public class FollowPlayer : MonoBehaviour
 {
     public GameObject PlayerTarget;
     public Vector3 offset;
-    public float smoothTime = 0.25f;
+    public float positionSmooth = 0.25f;
+    public float minZoom = 5.0f;
+    public float maxZoom = 10.0f;
+    public float maxVelocity = 15.0f;
+    public float zoomSmooth = 0.25f;
 
     private Rigidbody2D player_rb;
+    private Camera cam;
     private Vector3 currentVelocity;
+    private float currentSize;
 
     void Start(){
         player_rb = PlayerTarget.GetComponent<Rigidbody2D>();
+        cam = GetComponent<Camera>();
     }
     // Update is called once per frame
     void LateUpdate()
@@ -21,7 +28,12 @@ public class FollowPlayer : MonoBehaviour
                 transform.position,
                 PlayerTarget.transform.position + offset,
                 ref currentVelocity,
-                smoothTime
+                positionSmooth
                 );
+
+        float vel = Mathf.Clamp(player_rb.velocity.magnitude / maxVelocity, 0, 1);
+        float desiredSize = Mathf.Lerp(minZoom, maxZoom, vel);
+        cam.orthographicSize = Mathf.SmoothDamp(cam.orthographicSize, desiredSize, ref currentSize, zoomSmooth);
+        
     }
 }
