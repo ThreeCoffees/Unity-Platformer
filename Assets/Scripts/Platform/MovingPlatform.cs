6e42031b1888327f54d75fd4ba960843c8350039
ThreeCoffes <leonard.madzielewski@gmail.com>
2024-10-22 13:20:43 +0200

    moving platform working

--- Assets/Scripts/Platform/MovingPlatform.cs
+++ Assets/Scripts/Platform/MovingPlatform.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour
 {
+    [Range(0.1f, 30f)][SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private bool pingPong = false;
+    [SerializeField] Transform[] path;
+    private int index = 0;
+    private int direction = 1;
+    private const float distanceThreshold = 0.01f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = path[index].transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        NextPoint();
+        MoveTowardsNextPoint();
+    }
+
+    void MoveTowardsNextPoint(){
+        transform.position = Vector2.MoveTowards(transform.position, path[index].transform.position, moveSpeed * Time.deltaTime);
+    }
+
+    void NextPoint() {
+        if(Vector2.Distance(transform.position, path[index].transform.position) >= distanceThreshold) return;
+
+        index += direction;
+        if(index >= path.Length || index < 0) {
+            if(pingPong){
+                direction *= -1;
+                index += direction;
+            } else {
+                index = 0;
+            }
+        }
     }
 }
