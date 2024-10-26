fda949a4a5853b44308235ec71bd7888c8c96de8
gre-v-el <myszkier.gabriel@gmail.com>
2024-10-26 19:28:59 +0200

    Fixed lever handle animation jitter and added inspector parameters

--- Assets/Scripts/Lever/Lever.cs
+++ Assets/Scripts/Lever/Lever.cs
@@ -5,26 +5,40 @@ using UnityEngine.Events;
 
 public class Lever : MonoBehaviour
 {
-	private Rigidbody2D playerInBounds = null;
 	[SerializeField] private bool isRight = false;
-	private float currentHandleAngle = 0;
+	[SerializeField] [Range(0, 10)] private float pullVelocityThreshold = 3f;
+	[SerializeField] [Range(100, 2000)] private float pullAnimationSpeed = 500f;
 
 	[SerializeField] private GameObject HandleAnchor;
 	[SerializeField] private UnityEvent<bool> OnLeverPulled = new UnityEvent<bool>();
+	
+	private Rigidbody2D playerInBounds = null;
+	private float currentHandleAngle = 0;
+
+    private void Start()
+    {
+		if(isRight) currentHandleAngle = -90;
+    }
 
     void Update()
     {
+		// check if player pulls the lever
 		if (playerInBounds != null) {
 			bool prevIsRight = isRight;
 			Vector2 playerVelLocal = transform.InverseTransformDirection(playerInBounds.velocity);
-			if(Mathf.Abs(playerVelLocal.x) > 3f) isRight = playerVelLocal.x > 0;
+			if(Mathf.Abs(playerVelLocal.x) > pullVelocityThreshold) isRight = playerVelLocal.x > 0;
 			if(prevIsRight != isRight) OnLeverPulled.Invoke(isRight);
-
-			Debug.Log("Player velocity: " + playerVelLocal.x);
 		}
 
+		// animate lever handle
 		float targetAngle = isRight ? -90 : 0;
-		currentHandleAngle = Mathf.Sign(targetAngle - currentHandleAngle) * 500 * Time.deltaTime + currentHandleAngle;
+		
+		float angleDiff = targetAngle - currentHandleAngle;
+		float angleChange = Mathf.Sign(targetAngle - currentHandleAngle) * pullAnimationSpeed * Time.deltaTime;
+
+		if (Mathf.Abs(angleDiff) < Mathf.Abs(angleChange)) currentHandleAngle = targetAngle;
+		else currentHandleAngle += angleChange;
+
 		currentHandleAngle = Mathf.Clamp(currentHandleAngle, -90, 0);
 
         HandleAnchor.transform.localRotation = Quaternion.Euler(0, 0, currentHandleAngle);
