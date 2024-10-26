3321548873a7bebcce701148a9ac1751d3cb66db
ThreeCoffes <leonard.madzielewski@gmail.com>
2024-10-26 21:30:37 +0200

    wip

---
+++ Assets/Scripts/Wind.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindZone : MonoBehaviour
+{
+    [SerializeField] private float direction = 0.0f;
+    [SerializeField] private float strength = 30.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
